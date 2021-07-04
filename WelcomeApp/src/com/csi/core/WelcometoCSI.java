72fe01d6ed913fabf84de53122498540a56422b1
kiranjadhavcreditsystemsindia <admin@192.168.0.102>
2021-07-04 20:05:26 +0530

    Added latest changes

--- WelcomeApp/src/com/csi/core/WelcometoCSI.java
+++ WelcomeApp/src/com/csi/core/WelcometoCSI.java
@@ -2,6 +2,11 @@ package com.csi.core;
 
 public class WelcometoCSI {
 	public static void main(String[] args) {
-		System.out.println("WELCOME TO FOULL STACK PUNE");
+		System.out.println("INSTA CODE TECH- BEST PRODUCT BASE COMPANY IN PUNE");
+		show();
+	}
+
+	static void show() {
+		System.out.println("WELCOME TO FULL STACK PUNE");
 	}
 }
